38e3e19b9c829f79ae2d91558f9290a4f7c30016
Conrad Sulzer <56128916+ConradSulzer@users.noreply.github.com>
2025-10-20 23:29:51 -0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** u/type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./renderer/index.html', './renderer/src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
