4200a7314dfd3d7cef355bb7cdae7207e2b8845e
Conrad Sulzer <56128916+ConradSulzer@users.noreply.github.com>
2025-12-05 08:49:32 -0600

    remove react-query devtools

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { PreferencesProvider } from './context/preferences'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,7 +20,6 @@ createRoot(document.getElementById('root')!).render(
       <PreferencesProvider>
         <App />
       </PreferencesProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 )
